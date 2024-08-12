40486df5bf0e18c905f0befa9a8078d90206d252
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-08-12 06:57:18 -0700

    style: Add box shadow styles for different elevations

--- splitshifts-app/tailwind.config.ts
+++ splitshifts-app/tailwind.config.ts
@@ -75,6 +75,18 @@ const config: Config = {
         'title-md': ['16px', '24px'],
         'title-sm': ['14px', '20px'],
       },
+      boxShadow: {
+        'elevation-1':
+          '0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 1px 2px 0px rgba(0, 0, 0, 0.30);',
+        'elevation-2':
+          '0px 1px 2px 0px rgba(0, 0, 0, 0.30), 0px 2px 6px 2px rgba(0, 0, 0, 0.15)',
+        'elevation-3':
+          '0px 4px 8px 3px rgba(0, 0, 0, 0.15), 0px 1px 3px 0px rgba(0, 0, 0, 0.30)',
+        'elevation-4':
+          '0px 6px 10px 4px rgba(0, 0, 0, 0.15), 0px 2px 3px 0px rgba(0, 0, 0, 0.30)',
+        'elevation-5':
+          '0px 8px 12px 6px rgba(0, 0, 0, 0.15), 0px 4px 4px 0px rgba(0, 0, 0, 0.30)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
