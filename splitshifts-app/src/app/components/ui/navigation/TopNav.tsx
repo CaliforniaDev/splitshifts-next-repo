8c750c19bc1d9aa65315acbe72eacda6db2a0cf4
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-08-14 15:44:40 -0700

    chore: Update TopNav component with SplitShifts logo and navigation changes

--- splitshifts-app/src/app/components/ui/navigation/TopNav.tsx
+++ splitshifts-app/src/app/components/ui/navigation/TopNav.tsx
@@ -1,17 +1,32 @@
 import Image from 'next/image';
+import logo from '@/../public/assets/splitshifts-logo.svg';
+import { josefinSans } from '@/app/typeface/fonts';
 import Button from '@/app/components/ui/buttons/Button';
 
 export default function TopNav() {
-  return (
-    <nav className='flex h-20 items-center justify-between gap-4 px-[72px] py-[18px]'>
-      <div className='w-1/3'>
+  const Logo = () => {
+    return (
+      <div className='flex w-1/3 items-center gap-2'>
         <Image
-          src='/splitshifts-logo.svg'
+          src={logo}
           alt='SplitShifts Logo'
           width={48}
           height={48}
+          quality={100}
+          priority
         />
+        <span
+          className={`${josefinSans.className} pt-1.5 text-2xl font-bold text-inverse-surface`}
+        >
+          SplitShifts
+        </span>
       </div>
+    );
+  };
+
+  return (
+    <nav className='flex h-20 items-center justify-between gap-4 px-[72px] py-[18px]'>
+      <Logo />
       <div className='flex w-full justify-center'>
         <ul className='typescale-label-large z-10 flex min-w-max max-w-max gap-14 rounded-full border border-outline-variant px-6 py-2.5'>
           <li className='typescale-label-large-prominent text-primary'>
@@ -28,7 +43,7 @@ export default function TopNav() {
           </li>
         </ul>
       </div>
-      <div className='call-to-action flex w-1/3 gap-4'>
+      <div data-section='call-to-action' className='flex w-1/3 gap-4'>
         <Button variant='text'> Log In</Button>
         <Button variant='filled'>Start for free</Button>
       </div>
