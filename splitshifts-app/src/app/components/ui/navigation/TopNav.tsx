ed9a220c7cd9d772d2e847e9e86a34fa920ec1c9
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-08-14 05:57:32 -0700

    feat: Add SplitShifts logo to TopNav component

--- splitshifts-app/src/app/components/ui/navigation/TopNav.tsx
+++ splitshifts-app/src/app/components/ui/navigation/TopNav.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
 import Button from '@/app/components/ui/buttons/Button';
 
 export default function TopNav() {
   return (
-    <nav className='flex h-20 items-center justify-between gap-4 px-[72px]'>
+    <nav className='flex h-20 items-center justify-between gap-4 px-[72px] py-[18px]'>
       <div className='w-1/3'>
-        <h1>SplitShifts</h1>
+        <Image
+          src='/splitshifts-logo.svg'
+          alt='SplitShifts Logo'
+          width={48}
+          height={48}
+        />
       </div>
       <div className='flex w-full justify-center'>
         <ul className='typescale-label-large z-10 flex min-w-max max-w-max gap-14 rounded-full border border-outline-variant px-6 py-2.5'>
