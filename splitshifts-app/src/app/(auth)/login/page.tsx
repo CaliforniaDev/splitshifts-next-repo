5285a7a56e40144e5e8f38f05355d45ae0129a0e
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-09-05 03:14:08 -0700

    feat: Add login and signup routes test pages

---
+++ splitshifts-app/src/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+export default function LogInRoute() {
+  return (
+    <div>Log In Route </div>
+  )
+}
---
+++ splitshifts-app/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,5 @@
+export default function SignUpRoute() {
+  return (
+    <div>Sign Up Route</div>
+  )
+}
