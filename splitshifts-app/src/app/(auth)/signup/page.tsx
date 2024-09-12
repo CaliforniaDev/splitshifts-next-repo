2fbc9bde664f6c665b25d2974ad07c1954380cf0
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-09-12 02:27:57 -0700

    feat: Start sign up form ui design

--- splitshifts-app/src/app/(auth)/signup/page.tsx
+++ splitshifts-app/src/app/(auth)/signup/page.tsx
@@ -1,5 +1,20 @@
-export default function SignUpRoute() {
+import Button from '@/app/components/ui/buttons/button';
+export default function SignUpPage() {
   return (
-    <div>Sign Up Route</div>
-  )
+    <form action=''>
+      <div>
+        <label htmlFor='name'>Name</label>
+        <input id='name' name='name' placeholder='Name' />
+      </div>
+      <div>
+        <label htmlFor='email'>Email</label>
+        <input id='email' name='email' type='email' placeholder='Email' />
+      </div>
+      <div>
+        <label htmlFor='password'>Password</label>
+        <input id='password' name='password' type='password' />
+      </div>
+      <Button variant='filled' type='submit'>Sign Up</Button>
+    </form>
+  );
 }
