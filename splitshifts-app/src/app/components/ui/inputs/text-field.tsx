71e316070238e5ffe8c430a4670ca0c440aae0f3
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-09-20 02:58:20 -0700

    feat: Add focus state to TextField component

--- splitshifts-app/src/app/components/ui/inputs/text-field.tsx
+++ splitshifts-app/src/app/components/ui/inputs/text-field.tsx
@@ -1,6 +1,7 @@
 'use client';
 import clsx from 'clsx';
 import styles from './text-field.module.css';
+import { useState } from 'react';
 
 interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -22,33 +23,50 @@ export default function TextField({
   className,
   ...props
 }: TextFieldProps) {
+  const [isFocused, setIsFocused] = useState(false);
   const id = props.id || name;
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
   return (
     <div className='relative'>
-      <div
-        className={clsx(
-          styles.hoverStateLayer,
-          'relative w-full text-on-surface-variant',
-        )}
-      >
+      <span className={clsx(styles.hoverStateLayer, 'relative block w-full')}>
         <input
           id={id}
           value={value}
           name={name}
           onChange={onChange}
-          className={clsx(
-            className,
-            'relative h-14 rounded-t-[4px] border-b-[1px] border-on-surface bg-surface-container-highest',
-          )}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
           {...props}
+          className={clsx(
+            'relative h-14 w-full rounded-t-[4px] bg-surface-container-highest px-4 pb-2 pt-6 text-on-surface caret-primary transition-textfield duration-200 ease-in focus:outline-none',
+            {
+              className,
+              'border-b-2 border-b-primary': isFocused,
+              'border-b-[1px] border-b-on-surface': !isFocused,
+            },
+          )}
         />
         {/**
          * TODO: Add label styling
          */}
-        <label htmlFor={id} className=''>
+        <label
+          htmlFor={id}
+          className={clsx(
+            'pointer-events-none absolute left-4 right-4 text-left transition-all duration-300 ease-in-out',
+            {
+              'typescale-body-small pt-[8px] text-primary':
+                isFocused || value !== '',
+              'typescale-body-large pt-4 text-on-surface-variant':
+                !isFocused || value === '',
+            },
+          )}
+        >
           {label}
         </label>
-      </div>
+      </span>
     </div>
   );
 }
