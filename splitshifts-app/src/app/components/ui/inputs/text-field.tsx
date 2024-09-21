7f4ea2005008ea88cd6c3ed96a4c71e6e827f6a2
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-09-21 04:05:18 -0700

    refactor: TextField component styles and add accessibility attributes

--- splitshifts-app/src/app/components/ui/inputs/text-field.tsx
+++ splitshifts-app/src/app/components/ui/inputs/text-field.tsx
@@ -17,14 +17,20 @@ interface TextFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export default function TextField({
   label,
-  value,
+  value = '',
   name,
   onChange,
   className,
   ...props
 }: TextFieldProps) {
   const [isFocused, setIsFocused] = useState(false);
+  let isPopulated = value !== '';
   const id = props.id || name;
+  const errorId = `${id}-error`;
+  const supportingTextId = `${id}-supporting-text`;
+  const describedByIds = [];
+  if (errorId) describedByIds.push(errorId);
+  if (supportingTextId) describedByIds.push(supportingTextId);
 
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
@@ -39,27 +45,26 @@ export default function TextField({
           onChange={onChange}
           onFocus={handleFocus}
           onBlur={handleBlur}
+          // aria-invalid={!!error}
+          aria-describedby={
+            describedByIds.length > 0 ? describedByIds.join(' ') : undefined
+          }
           {...props}
-          className={clsx(
-            'relative h-14 w-full rounded-t-[4px] bg-surface-container-highest px-4 pb-2 pt-6 text-on-surface caret-primary transition-textfield duration-200 ease-in focus:outline-none',
-            className,
-            {
-              'border-b-2 border-b-primary': isFocused,
-              'border-b-[1px] border-b-on-surface': !isFocused,
-            },
-          )}
+          className={clsx(styles.inputBase, {
+            'border-b-2 border-b-primary': isFocused, // && !error,w
+            'border-b-[1px] border-b-on-surface': !isFocused,
+            // 'border-b-error': error,
+          })}
         />
         <label
           htmlFor={id}
-          className={clsx(
-            'pointer-events-none absolute left-4 right-4 text-left transition-all duration-300 ease-in-out',
-            {
-              'typescale-body-small pt-[8px] text-primary':
-                isFocused || value !== '',
-              'typescale-body-large pt-4 text-on-surface-variant':
-                !isFocused || value === '',
-            },
-          )}
+          className={clsx(styles.labelBase, {
+            'text-primary': isFocused, // && !error,
+            'text-on-surface-variant': !isFocused,
+            'typescale-body-large pt-4': !isFocused && !isPopulated,
+            'typescale-body-small pt-[8px]': isFocused || isPopulated,
+            // 'text-on-error-container': error,
+          })}
         >
           {label}
         </label>
