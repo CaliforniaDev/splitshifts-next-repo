428cb9a412a782e378a646eed597727e3286f194
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-02-13 05:56:54 -0800

    feat: Update NextAuth configuration to include Auth0 options for improved authentication

--- splitshifts-app/src/lib/auth.ts
+++ splitshifts-app/src/lib/auth.ts
@@ -1,22 +1,20 @@
-'use server';
+
 
 import NextAuth from 'next-auth';
 import Auth0 from 'next-auth/providers/auth0';
 import PostgresAdapter from '@auth/pg-adapter';
 import { Pool } from '@neondatabase/serverless';
 
-export const { handlers, signIn, signOut, auth } = NextAuth(() => {
+export const { handlers, signIn, signOut, auth } = NextAuth(async () => {
   const pool = new Pool({ connectionString: process.env.DATA_BASE_URL });
   return {
     adapter: PostgresAdapter(pool),
-    providers: [Auth0],
+    providers: [
+      Auth0({
+        clientId: process.env.AUTH_AUTH0_ID,
+        clientSecret: process.env.AUTH_AUTH0_SECRET,
+        issuer: process.env.AUTH_AUTH0_ISSUER,
+      }),
+    ],
   };
 });
-
-// ({
-//       clientId: process.env.AUTH_AUTH0_ID,
-//       clientSecret: process.env.AUTH_AUTH0_SECRET,
-//       issuer: process.env.AUTH_AUTH0_ISSUER,
-//     }),
-//   ],
-// }
