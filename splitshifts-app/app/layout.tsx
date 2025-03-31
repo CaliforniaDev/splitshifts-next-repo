7e20b4f2507e0eadd1b8277612cf34a25e78f6cc
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-03-30 20:34:27 -0700

    refactor(navigation): organize nav file to make room for new dashboard navigation nesting each item in separate folders

--- splitshifts-app/app/layout.tsx
+++ splitshifts-app/app/layout.tsx
@@ -1,7 +1,9 @@
-import type { Metadata } from 'next';
 import { inter, spaceGrotesk } from '@/app/typeface/fonts';
+import type { Metadata } from 'next';
 
-import TopNav from '@/app/components/ui/landing-page/navigation/top-nav';
+import TopNav from '@/app/components/ui/nav/landing/top-nav';
+import { auth } from '@/auth';
+import LogoutButton from './components/ui/auth/logout-button';
 import './globals.css';
 import './typeface/typography.css';
 
@@ -11,17 +13,28 @@ export const metadata: Metadata = {
     'A web application designed to streamline the scheduling process for organizations.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html
       lang='en'
       className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
     >
       <body className={`${inter.className} antialiased`}>
+        <div>
+          {session?.user?.email ? (
+            <div>
+              {session.user.email}
+              <LogoutButton>Logout</LogoutButton>
+            </div>
+          ) : (
+            'No Current user logged in'
+          )}
+        </div>
         <TopNav />
         {children}
       </body>
