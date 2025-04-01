1a7fd2afc11411951de7534cf3700d9dc989e9da
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-01 02:55:05 -0700

    refactor(layout): simplify RootLayout by removing authentication logic and LogoutButton

--- splitshifts-app/app/layout.tsx
+++ splitshifts-app/app/layout.tsx
@@ -3,9 +3,6 @@ import './typeface/typography.css';
 import type { Metadata } from 'next';
 import { inter, spaceGrotesk } from '@/app/typeface/fonts';
 
-import { auth } from '@/auth';
-import LogoutButton from './components/ui/auth/logout-button';
-
 export const metadata: Metadata = {
   title: 'SplitShifts',
   description:
@@ -17,25 +14,12 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
   return (
     <html
       lang='en'
       className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
     >
-      <body className={`${inter.className} antialiased`}>
-        <div>
-          {session?.user?.email ? (
-            <div>
-              {session.user.email}
-              <LogoutButton>Logout</LogoutButton>
-            </div>
-          ) : (
-            'No Current user logged in'
-          )}
-        </div>
-        {children}
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
