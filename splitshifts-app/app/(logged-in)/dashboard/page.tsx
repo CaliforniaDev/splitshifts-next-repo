cbd344cc56b1d384e0c29790dc313749b2d593f7
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-02 16:54:47 -0700

    feat(dashboard): integrate authentication and update layout with user email display

--- splitshifts-app/app/(logged-in)/dashboard/page.tsx
+++ splitshifts-app/app/(logged-in)/dashboard/page.tsx
@@ -1,11 +1,30 @@
 // File: app/(logged-in)/dashboard/page.tsx
 
-export default function Dashboard() {
+import { auth } from '@/auth';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/app/components/ui/card';
+import { Label } from '@/app/components/ui/label';
+
+export default async function Dashboard() {
+  const session = await auth();
   return (
-    <section className='flex flex-1 rounded-xl bg-surface-container-low p-4'>
-      <div className=''>
-        <h1 className='typescale-title-large'>Hello Dashboard</h1>
-      </div>
-    </section>
+    // <section className='flex flex-1 rounded-xl bg-surface-container-low p-4'>
+    //   <div className=''>
+    //     <h1 className='typescale-title-large'>Hello Dashboard</h1>
+    //   </div>
+    // </section>
+    <Card className='w-[350px]'>
+      <CardHeader>
+        <CardTitle>Hello Dashboard</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Label>Email Address</Label>
+        <div className='text-on-surface-variant'>{session?.user?.email}</div>
+      </CardContent>
+    </Card>
   );
 }
