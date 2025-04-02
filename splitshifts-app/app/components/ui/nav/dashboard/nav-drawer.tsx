b5f232369960b9a5e28a093c5cacdcd86b92f7bf
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-01 17:55:59 -0700

    feat(nav): refactor NavDrawer to use List and ListItem components for improved structure and readability

--- splitshifts-app/app/components/ui/nav/dashboard/nav-drawer.tsx
+++ splitshifts-app/app/components/ui/nav/dashboard/nav-drawer.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import { Home, Settings, LogOut, Key } from 'lucide-react';
 import { logOut } from '@/app/(public)/(auth)/actions/logout';
 
+import Logo from '@/app/components/ui/logo';
+import { List, ListItem, ListItemLink } from './nav-list';
+
 export default function NavDrawer() {
   const handleLogout = async () => {
     try {
@@ -14,48 +17,39 @@ export default function NavDrawer() {
   };
   return (
     <aside className='h-screen w-64 bg-surface-container-low text-on-surface-variant'>
-      <div className='flex h-full flex-col space-y-4 px-4 py-6'>
-        <div className='text-xl font-semibold'>SplitShifts</div>
+      <div className='flex h-full flex-col space-y-4 px-3 py-[38px]'>
+        <div className='flex h-14 text-xl font-semibold px-4'>
+          <Logo width={38} height={38} textClassName='text-xl'/>
+        </div>
 
-        <nav className='flex flex-col space-y-2'>
-          <ul role='navigation' className='*:border *:border-red-300'>
-            <li className='border border-red-300'>
-              <Link
-                href='/dashboard'
-                className='flex items-center gap-3 rounded-lg px-3 py-2 transition hover:bg-surface-container'
-              >
-                <Home className='h-5 w-5' />
-                <span>Dashboard</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href='/change-password'
-                className='flex items-center gap-3 rounded-lg px-3 py-2 transition hover:bg-surface-container'
-              >
-                <Key className='h-5 w-5' />
-                <span>Change Password</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href='/settings'
-                className='flex items-center gap-3 rounded-lg px-3 py-2 transition hover:bg-surface-container'
-              >
-                <Settings className='h-5 w-5' />
-                <span>Settings</span>
-              </Link>
-            </li>
-            <li>
-              <button
-                className='mt-auto flex w-full items-center gap-3 rounded-lg px-3 py-2 transition hover:bg-surface-container'
-                onClick={handleLogout}
-              >
-                <LogOut className='h-5 w-5' />
-                <span>Logout</span>
-              </button>
-            </li>
-          </ul>
+        <nav className='flex h-full flex-col justify-between space-y-2'>
+          <List>
+            <ListItemLink
+              href='/dashboard'
+              label='Dashboard'
+              icon={<Home className='h-5 w-5' />}
+              className='bg-secondary-container text-on-secondary-container'
+            />
+            <ListItemLink
+              href='/change-password'
+              label='Change Password'
+              icon={<Key className='h-5 w-5' />}
+            />
+            <ListItemLink
+              href='/settings'
+              label='Settings'
+              icon={<Settings className='h-5 w-5' />}
+            />
+          </List>
+          <div>
+            <button
+              className='mt-auto flex w-full items-center gap-3 rounded-lg px-3 py-2 transition hover:bg-surface-container'
+              onClick={handleLogout}
+            >
+              <LogOut className='h-5 w-5' />
+              <span>Logout</span>
+            </button>
+          </div>
         </nav>
       </div>
     </aside>
