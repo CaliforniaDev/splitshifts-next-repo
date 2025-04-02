8038cba3fa4ad94b041650bf23d2643edaa14ce1
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-01 17:55:40 -0700

    feat(logo): enhance Logo component with customizable width, height, and class names

--- splitshifts-app/app/components/ui/logo.tsx
+++ splitshifts-app/app/components/ui/logo.tsx
@@ -1,20 +1,40 @@
+import { cn } from '@/app/lib/utils';
 import Image from 'next/image';
 import { josefinSans } from '@/app/typeface/fonts';
 import logo from '@/public/assets/splitshifts-logo.svg';
 
-export default function Logo() {
+interface LogoProps {
+  width?: number;
+  height?: number;
+  className?: string;
+  textClassName?: string;
+}
+
+export default function Logo({
+  width = 48,
+  height = 48,
+  className,
+  textClassName,
+}: LogoProps) {
   return (
-    <div aria-label='Logo' className='flex w-1/3 items-center gap-2'>
+    <div
+      aria-label='Logo'
+      className={cn('flex w-1/3 items-center gap-2', className)}
+    >
       <Image
         src={logo}
         alt='SplitShifts Logo'
-        width={48}
-        height={48}
+        width={width}
+        height={height}
         quality={100}
         priority
       />
       <span
-        className={`${josefinSans.className} pt-1.5 text-2xl font-bold text-inverse-surface`}
+        className={cn(
+          josefinSans.className,
+          'pt-1.5 text-2xl font-bold text-inverse-surface',
+          textClassName,
+        )}
       >
         SplitShifts
       </span>
