e23b5e46ddfb689ed9f82c5dc996759ba01eabcd
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-02 06:10:49 -0700

    feat(layout): implement authentication check in LoggedIn and Public layouts

--- splitshifts-app/app/(logged-in)/layout.tsx
+++ splitshifts-app/app/(logged-in)/layout.tsx
@@ -1,11 +1,19 @@
 // File: app/(logged-in)/layout,
 
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 import NavDrawer from '@/app/components/ui/nav/dashboard/nav-drawer';
+
+
 export default async function LoggedInLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+  }) {
+  const session = await auth();
+  if (!session?.user?.id) {
+    redirect('/');
+  }
   return (
     <div className='flex min-h-screen'>
       <NavDrawer />
--- splitshifts-app/app/(public)/layout.tsx
+++ splitshifts-app/app/(public)/layout.tsx
@@ -1,10 +1,18 @@
+// File: app/(public)/layout.tsx
+
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 import { LandingNav } from '@/app/components/ui/nav/landing';
 
-export default function PublicLayout({
+export default async function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+  if (!!session?.user?.id) {
+    redirect('/dashboard');
+  }
   return (
     <>
       <LandingNav />
