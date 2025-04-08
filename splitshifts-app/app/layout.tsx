b3b6c7724617f246e9ac8b9fcebb3219fa713a6d
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-08 06:10:03 -0700

    feat(layout): add Toaster component to RootLayout for toast notifications

--- splitshifts-app/app/layout.tsx
+++ splitshifts-app/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import './typeface/typography.css';
 import type { Metadata } from 'next';
 import { inter, spaceGrotesk } from '@/app/typeface/fonts';
+import { Toaster } from './components/ui/toast';
 
 export const metadata: Metadata = {
   title: 'SplitShifts',
@@ -19,7 +20,10 @@ export default async function RootLayout({
       lang='en'
       className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
     >
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
