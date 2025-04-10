729456b2e90c8db6b7a0beec51a151cec2ed3843
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-04-10 05:44:17 -0700

    feat(password-reset): set default email in Reset Password form from query parameters

--- splitshifts-app/app/(public)/(auth)/password-reset/hooks/useResetForm.ts
+++ splitshifts-app/app/(public)/(auth)/password-reset/hooks/useResetForm.ts
@@ -1,13 +1,16 @@
 import { useForm } from 'react-hook-form';
+import { useSearchParams } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { resetPasswordSchema } from '../../validation/auth-schema';
 import { ResetPasswordFormData } from '../types/reset-password-form-data';
 
 export const useResetPasswordForm = () => {
+  const searchParams = useSearchParams();
+  const defaultEmail = decodeURIComponent(searchParams.get('email') ?? '');
   return useForm<ResetPasswordFormData>({
     resolver: zodResolver(resetPasswordSchema),
     defaultValues: {
-      email: '',
+      email: defaultEmail,
     },
   });
 };
