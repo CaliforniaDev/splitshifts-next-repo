806952111dc66ca3b11ee2004acfffcd89813334
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-07-24 01:08:33 -0700

    chore(update-password): remove unused imports from update password form data type

--- splitshifts-app/app/(public)/(auth)/update-password/types/update-password-form-data.ts
+++ splitshifts-app/app/(public)/(auth)/update-password/types/update-password-form-data.ts
@@ -1,6 +1,4 @@
 import { z } from 'zod';
-// import { useUpdatePasswordForm } from '../hooks/use-update-password-form';
-// import { updatePasswordSchema } from '../validation/update-password-schema';
 import { passwordMatchSchema } from '../../validation/auth-schema';
 
 // This type is used to strongly type the form data inside useForm<>() hook
