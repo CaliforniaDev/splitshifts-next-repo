f13d598fdfa398d44565ab74a6c7602305cdc9b6
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2025-09-09 19:06:39 -0700

    fix: correct typo

--- splitshifts-app/app/manifest.ts
+++ splitshifts-app/app/manifest.ts
@@ -15,19 +15,19 @@ export default function manifest(): MetadataRoute.Manifest {
     lang: 'en',
     icons: [
       {
-        src: '/andriod-chrome-icons/icon-192.png',
+        src: '/android-chrome-icons/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/andriod-chrome-icons/icon-512.png',
+        src: '/android-chrome-icons/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/andriod-chrome-icons/icon-512.png',
+        src: '/android-chrome-icons/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
